aafb98e1a8a612299b5a2d64d8f150a8606bed95
Hoang Anh Pham - Framgia PC <pham.hoang.anh@framgia.com>
2019-03-01 09:29:43 +0700

    Add Devices API

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from api.views.total_view import Total
 from api.views.timerange_total_view import TimerangeTotal
+from api.views.devices_view import Devices
 
 app_name = 'api'
 
 urlpatterns = [
     path('total', Total.as_view(), name='total-page-views'),
     path('timerange_total', TimerangeTotal.as_view(), name='timerange-total-page-views'),
+    path('devices', Devices.as_view(), name='devices-page-views'),
 ]
